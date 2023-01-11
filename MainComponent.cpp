6f902c1bb60dcad7fddf2ffba10b28ce5d140cea
fhitzke <87448209+fhitzke@users.noreply.github.com>
2023-01-11 18:39:01 +0100

    Add files via upload

---
+++ MainComponent.cpp
@@ -0,0 +1,84 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    // This function will be called when the audio device is started, or when
+    // its settings (i.e. sample rate, block size, etc) are changed.
+
+    // You can use this function to initialise any resources you might need,
+    // but be careful - it will be called on the audio thread, not the GUI thread.
+
+    // For more details, see the help for AudioProcessor::prepareToPlay()
+}
+
+//-------------------------------------------------------------------------------
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+    bufferToFill.clearActiveBufferRegion();
+    
+    auto* buffer = bufferToFill.buffer; // setup variable, buffer itself
+    auto numChannels = buffer->getNumChannels(); // using pointer to get num
+    auto numSamples = buffer->getNumSamples(); // for channels and samples
+    
+    for (auto channel = 0; channel < numChannels; ++channel) // iterate through channels
+    {
+        auto* writePointer = buffer->getWritePointer(channel); // access to channel
+        for (auto sample = 0; sample < numSamples; ++sample) // iterate through samples
+        {
+            writePointer[sample] = randomGenerator.nextFloat()*0.1f; // get floating number between 0 exlusive 1
+        }
+    }
+}
+//-------------------------------------------------------------------------------
+
+void MainComponent::releaseResources()
+{
+    // This will be called when the audio device stops, or when it is being
+    // restarted due to a setting change.
+
+    // For more details, see the help for AudioProcessor::releaseResources()
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
